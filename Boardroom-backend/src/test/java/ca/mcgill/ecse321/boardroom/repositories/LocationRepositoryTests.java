147e69046e5580f97e4e9dfac95a6efebd4644d1
Janelle Tam <janelle.tam@mail.mcgill.ca>
2025-02-13 11:10:04 -0500

    Finish location test'

--- Boardroom-backend/src/test/java/ca/mcgill/ecse321/boardroom/repositories/LocationRepositoryTests.java
+++ Boardroom-backend/src/test/java/ca/mcgill/ecse321/boardroom/repositories/LocationRepositoryTests.java
@@ -1,5 +1,42 @@
 package ca.mcgill.ecse321.boardroom.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.boardroom.model.Location;
+
+@SpringBootTest
 public class LocationRepositoryTests {
-	
+    @Autowired
+	private LocationRepository repo;
+
+	@AfterEach
+	public void clearDatabase() {
+		repo.deleteAll();
+	}
+
+	@Test
+	public void testCreateAndReadLocation() {
+		// Arrange
+        String address = "1234 Rue Sainte-Catherine";
+        String city = "Montreal";
+        String province = "Quebec";
+        Location montrealCafe = new Location(address, city, province);
+        montrealCafe = repo.save(montrealCafe);
+
+		// Act
+		Location cafeFromDB = repo.findLocationById(montrealCafe.getId());
+
+		// Assert
+		assertNotNull(cafeFromDB);
+		assertEquals(cafeFromDB.getId(), montrealCafe.getId());
+		assertEquals(cafeFromDB.getAddress(), montrealCafe.getAddress());
+		assertEquals(cafeFromDB.getCity(), montrealCafe.getCity());
+		assertEquals(cafeFromDB.getProvince(), montrealCafe.getProvince());
+	}
 }

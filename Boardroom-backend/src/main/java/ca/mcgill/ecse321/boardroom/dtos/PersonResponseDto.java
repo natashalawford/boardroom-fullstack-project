629cdcde913afc0b263c2bab5fb263dd54f4cc02
Jason Rustom <jasonrustom04@gmail.com>
2025-03-04 10:41:16 -0500

    Added person request and person response dtos

---
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/dtos/PersonResponseDto.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse321.boardroom.dtos;
+
+import ca.mcgill.ecse321.boardroom.model.Person;
+
+public class PersonResponseDto {
+    int id;
+    String name;
+    String email;
+    boolean owner;
+
+    private PersonResponseDto() {}
+
+    public PersonResponseDto(Person person) {
+        this.id = person.getId();
+        this.name = person.getName();
+        this.email = person.getEmail();
+        this.owner = person.isOwner();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isOwner() {
+        return owner;
+    }
+}

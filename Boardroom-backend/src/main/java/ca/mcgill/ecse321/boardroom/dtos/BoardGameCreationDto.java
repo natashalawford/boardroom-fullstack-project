af0e5fcef254bf390fdc3a5aa6d7cc3234c29078
Janelle Tam <janelle.tam@mail.mcgill.ca>
2025-03-05 08:45:31 -0500

    Add board game + specific board game creation dtos

---
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/dtos/BoardGameCreationDto.java
@@ -0,0 +1,42 @@
+package ca.mcgill.ecse321.boardroom.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+
+public class BoardGameCreationDto {
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @Min(value = 1, message = "Number of players must be at least 1.")
+    private int playersNeeded;
+
+    @NotBlank(message = "A photo of the board game is required")
+    private int picture;
+
+    public BoardGameCreationDto(String title, String description,
+                            int playersNeeded, int picture) {
+        this.title = title;
+        this.description = description;
+        this.playersNeeded = playersNeeded;
+        this.picture = picture;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() { 
+        return description; 
+    }
+
+    public int getPlayersNeeded() {
+        return playersNeeded;
+    }
+
+    public int getPicture() {
+        return picture;
+    }
+}
--- Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/dtos/BorrowRequestDtoCreation.java
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/dtos/BorrowRequestDtoCreation.java
@@ -7,7 +7,7 @@ import ca.mcgill.ecse321.boardroom.model.Person;
 import ca.mcgill.ecse321.boardroom.model.SpecificBoardGame;
 import ca.mcgill.ecse321.boardroom.model.enums.RequestStatus;
 import ca.mcgill.ecse321.boardroom.model.BorrowRequest;
-
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 

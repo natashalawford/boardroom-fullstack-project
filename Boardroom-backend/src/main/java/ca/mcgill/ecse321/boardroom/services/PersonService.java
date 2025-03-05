ecde5390c80dafaec8e83c75246fa82078fee081
Jason Rustom <jasonrustom04@gmail.com>
2025-03-04 23:22:39 -0500

    Removed unnecessary warning suppression

--- Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/services/PersonService.java
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/services/PersonService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 public class PersonService {
 
     @Autowired
-    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
     private PersonRepository personRepo;
 
 

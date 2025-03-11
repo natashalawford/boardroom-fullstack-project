0139c74be4ded63042d2cd553dd0b42be0d6681b
Jason Rustom <jasonrustom04@gmail.com>
2025-03-11 12:44:21 -0400

    added create method to endpoint for testing purposes (also just need it)

--- Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/controller/PersonController.java
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/controller/PersonController.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.boardroom.controller;
 
+import ca.mcgill.ecse321.boardroom.dtos.PersonCreationDto;
 import ca.mcgill.ecse321.boardroom.dtos.PersonRequestDto;
 import ca.mcgill.ecse321.boardroom.dtos.PersonUpdateDto;
 import ca.mcgill.ecse321.boardroom.dtos.responses.PersonResponseDto;
@@ -10,8 +11,10 @@ import ca.mcgill.ecse321.boardroom.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
  
@@ -19,13 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @Autowired
     private PersonService personService;
- 
+
+    
+    @PostMapping("people")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PersonResponseDto createPerson(@RequestBody PersonCreationDto personToCreate) {
+        return new PersonResponseDto(personService.createPerson(personToCreate));
+    }
 
     /**
      * Update person type
      */
-    @PutMapping("person/{id}/role")
-    public PersonResponseDto toggleAccountType(@PathVariable int id,
+    @PutMapping("people/{id}/role")
+    public PersonResponseDto toggleAccountType(@PathVariable("id") int id,
                                               @RequestBody PersonRequestDto partialUpdatedPerson) {
         //Get rest of information of person from database
         Person personToUpdate =

b7e231ff8633e2b3db38e42b42c11bbc54377d57
Nathan Reid <149529301+NathanRReid@users.noreply.github.com>
2025-03-13 16:07:13 -0400

    fixing minor issues with syntax highlighting and imports

--- Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/controller/BorrowController.java
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/controller/BorrowController.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.boardroom.controller;
 
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ca.mcgill.ecse321.boardroom.dtos.BorrowRequestDtoCreation;
 import ca.mcgill.ecse321.boardroom.dtos.BorrowRequestDtoSpecific;
 import ca.mcgill.ecse321.boardroom.model.BorrowRequest;
+import ca.mcgill.ecse321.boardroom.model.enums.RequestStatus;
 import ca.mcgill.ecse321.boardroom.services.BorrowService;
 
 
@@ -30,17 +32,17 @@ public class BorrowController {
     @PutMapping("/borrowRequests/{id}")
     @ResponseStatus(HttpStatus.OK)
     public BorrowRequestDtoSpecific updateBorrowRequest(@PathVariable int id, @RequestBody RequestStatus status){
-        BorrowRequest updatedBorrowRequest = borrowService.updateBorrowRequest(id, status);
-        return new BorrowRequestDtoSpecific(updateBorrowRequest);
+        BorrowRequest updatedBorrowRequest = borrowService.updateBorrowRequestStatus(id, status);
+        return new BorrowRequestDtoSpecific(updatedBorrowRequest);
     }
 
     @GetMapping("/borrowRequests")
     @ResponseStatus(HttpStatus.OK)
     public List<BorrowRequestDtoSpecific> viewPendingBorrowRequests(){
-        ArrayList<BorrowRequestDtoSpecific> borrowRequestDtos = new ArrayList<>();
-        ArrayList<BorrowRequest> borrowRequests = borrowService.viewPendingBorrowRequests();
+        List<BorrowRequestDtoSpecific> borrowRequestDtos = new ArrayList<>();
+        List<BorrowRequest> borrowRequests = borrowService.viewPendingBorrowRequests();
         for(BorrowRequest borrowRequest : borrowRequests){
-            borrowRequestDtos.add(new BorrowRequestDtoSpecific(borrowRequest.getStatus(), borrowRequest.getRequestStartDate(), borrowRequest.getRequestEndDate(), borrowRequest.getPerson().getId(), borrowRequest.getSpecificBoardGame().getId()));
+            borrowRequestDtos.add(new BorrowRequestDtoSpecific(borrowRequest));
         }
         return borrowRequestDtos;
     }

bc782652eaab79e0b94ffa8e0b738e6c7c9f0d6a
Nathan Reid <149529301+NathanRReid@users.noreply.github.com>
2025-03-13 12:58:32 -0400

    adding rest endpoint for create borrow request

---
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/controller/BorrowController.java
@@ -0,0 +1,30 @@
+package ca.mcgill.ecse321.boardroom.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.boardroom.dtos.BorrowRequestDtoCreation;
+import ca.mcgill.ecse321.boardroom.dtos.BorrowRequestDtoSpecific;
+import ca.mcgill.ecse321.boardroom.model.BorrowRequest;
+import ca.mcgill.ecse321.boardroom.services.BorrowService;
+
+
+public class BorrowController {
+    @Autowired
+    private BorrowService borrowService;
+
+    @PostMapping("/borrowRequests")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BorrowRequestDtoSpecific createBorrowRequest(@RequestBody BorrowRequestDtoCreation borrowRequestToCreate){
+        BorrowRequest createdBorrowRequest = borrowService.createBorrowRequest(borrowRequestToCreate);
+        return new BorrowRequestDtoSpecific(createdBorrowRequest);
+    }
+
+}

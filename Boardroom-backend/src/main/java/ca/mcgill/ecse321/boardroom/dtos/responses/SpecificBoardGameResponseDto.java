2ec62bc32ccbfd7580ad65fb62bb0fd1038d1641
Jason Rustom <jasonrustom04@gmail.com>
2025-03-13 00:23:59 -0400

    controller for game owner

---
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/dtos/responses/SpecificBoardGameResponseDto.java
@@ -0,0 +1,48 @@
+package ca.mcgill.ecse321.boardroom.dtos.responses;
+
+import ca.mcgill.ecse321.boardroom.model.SpecificBoardGame;
+import ca.mcgill.ecse321.boardroom.model.enums.GameStatus;
+
+public class SpecificBoardGameResponseDto {
+    private int id;
+    private String description;
+    private int picture;
+    private GameStatus status;
+    private String boardGameTitle;
+
+    //might not need this since the request is coming from an owner, so id is already knwon
+    int ownerId;
+
+    public SpecificBoardGameResponseDto(SpecificBoardGame specificBoardGame) {
+        this.id = specificBoardGame.getId();
+        this.description = specificBoardGame.getDescription();
+        this.picture = specificBoardGame.getPicture();
+        this.status = specificBoardGame.getStatus();
+        this.boardGameTitle = specificBoardGame.getBoardGame().getTitle();
+        this.ownerId = specificBoardGame.getOwner().getId();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPicture() {
+        return picture;
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public String getBoardGameTitle() {
+        return boardGameTitle;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    } 
+}

88f6e0f0954ba2f4bb83b3562bc33b0ff58bab9c
Jason Rustom <jasonrustom04@gmail.com>
2025-03-17 11:00:29 -0400

    added jakarta validation to relevant person dtos

--- Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/controller/PersonController.java
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/controller/PersonController.java
@@ -4,9 +4,12 @@ import ca.mcgill.ecse321.boardroom.dtos.PersonCreationDto;
 import ca.mcgill.ecse321.boardroom.dtos.PersonRequestDto;
 import ca.mcgill.ecse321.boardroom.dtos.responses.PersonResponseDto;
 import ca.mcgill.ecse321.boardroom.services.PersonService;
+import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,7 +26,7 @@ public class PersonController {
     
     @PostMapping("people")
     @ResponseStatus(HttpStatus.CREATED)
-    public PersonResponseDto createPerson(@RequestBody PersonCreationDto personToCreate) {
+    public PersonResponseDto createPerson(@Valid @RequestBody PersonCreationDto personToCreate) {
         return new PersonResponseDto(personService.createPerson(personToCreate));
     }
 
@@ -31,8 +34,17 @@ public class PersonController {
      * Update person type
      */
     @PutMapping("people/{id}/role")
-    public PersonResponseDto toggleAccountType(@PathVariable("id") int id,@RequestBody PersonRequestDto partialUpdatedPerson) {
-        
+    public PersonResponseDto toggleAccountType(@PathVariable("id") int id,@Valid @RequestBody PersonRequestDto partialUpdatedPerson) {
         return new PersonResponseDto(personService.updatePerson(id, partialUpdatedPerson));
+    }
+
+    @GetMapping("people/{id}") 
+    public PersonResponseDto getPerson(@PathVariable("id") int id) {
+        return new PersonResponseDto(personService.findPersonById(id));
+    }
+
+    @DeleteMapping("people/{id}")
+    public void deletePerson(@PathVariable("id") int id) {
+        personService.deletePerson(id);
     }
 }

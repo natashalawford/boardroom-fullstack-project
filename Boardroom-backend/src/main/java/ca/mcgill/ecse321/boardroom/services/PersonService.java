ad371ef0b1e53c8adf379f7ad87ed567e665f3ba
Jason Rustom <jasonrustom04@gmail.com>
2025-03-18 09:28:13 -0400

    reworked login

--- Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/services/PersonService.java
+++ Boardroom-backend/src/main/java/ca/mcgill/ecse321/boardroom/services/PersonService.java
@@ -1,15 +1,13 @@
 package ca.mcgill.ecse321.boardroom.services;
 
-import ca.mcgill.ecse321.boardroom.dtos.PersonCreationDto;
 import ca.mcgill.ecse321.boardroom.dtos.PersonLoginDto;
 import ca.mcgill.ecse321.boardroom.dtos.PersonRequestDto;
+import ca.mcgill.ecse321.boardroom.dtos.creation.PersonCreationDto;
 import ca.mcgill.ecse321.boardroom.dtos.responses.PersonResponseDto;
 import ca.mcgill.ecse321.boardroom.exceptions.BoardroomException;
 import ca.mcgill.ecse321.boardroom.model.Person;
 import ca.mcgill.ecse321.boardroom.repositories.PersonRepository;
 import jakarta.transaction.Transactional;
-import java.util.Optional;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,6 +28,7 @@ public class PersonService {
         return person;
     }
 
+    //THIS IS UPDATED TO HANDLE TRYING TO MAKE AN ACCOUNT WITH AN EMAIL THAT ALREADY EXISTS
     @Transactional
     public Person createPerson(PersonCreationDto personToCreate) {
         Person newPerson = new Person(personToCreate.getName(),
@@ -37,7 +36,11 @@ public class PersonService {
                 personToCreate.getPassword(),
                 personToCreate.isOwner());
 
-        return personRepo.save(newPerson);
+        try {
+            return personRepo.save(newPerson);
+        } catch (Exception e) {
+            throw new BoardroomException(HttpStatus.BAD_REQUEST, String.format("An error occured creating the account: %s", e.getMessage()));
+        }
     }
 
     @Transactional
@@ -56,40 +59,8 @@ public class PersonService {
                 personToUpdate.getPassword(), personToUpdateDto.isOwner());
 
         return personRepo.save(updatedPerson);
-    }
+    } 
 
-    //delete person
-    @Transactional
-    public void deletePersonById(int id) {
-        Person person = personRepo.findPersonById(id);
-        if (person == null) {
-            throw new BoardroomException(HttpStatus.NOT_FOUND,
-                String.format("No person has id %d", id));
-        }
-        personRepo.deleteById(id);
-    }
-
-    @Transactional
-    public PersonResponseDto login(PersonLoginDto loginDto) {
-        if (loginDto.getEmail() == null || loginDto.getPassword() == null) {
-            throw new BoardroomException(HttpStatus.BAD_REQUEST, "Email and password are required.");
-        }
-
-        // check for valid email, then password and throw error if invalid
-        Optional<Person> optionalPerson = personRepo.findByEmail(loginDto.getEmail());
-        if (optionalPerson.isEmpty()) {
-            throw new BoardroomException(HttpStatus.UNAUTHORIZED, "Invalid email or password.");
-        }
-
-        Person person = optionalPerson.get();
-        if (!person.getPassword().equals(loginDto.getPassword())) {
-            throw new BoardroomException(HttpStatus.UNAUTHORIZED, "Invalid email or password.");
-        }
-
-        return new PersonResponseDto(person);
-    }
-
-    //Maybe this should return a boolean or something
     @Transactional
     public void deletePerson(int id) {
         //Get person to delete
@@ -104,4 +75,19 @@ public class PersonService {
         personRepo.delete(personToDelete);
     }
 
+    public PersonResponseDto login(PersonLoginDto loginDto) { 
+        // Find person with email
+        Person existingPerson = personRepo.findByEmail(loginDto.getEmail());
+
+        if (null == existingPerson) {
+            throw new BoardroomException(HttpStatus.UNAUTHORIZED, "Invalid email or password.");
+        }
+
+        // Verify password
+        if (!existingPerson.getPassword().equals(loginDto.getPassword())) {
+            throw new BoardroomException(HttpStatus.UNAUTHORIZED, "Invalid email or password.");
+        }
+
+        return new PersonResponseDto(existingPerson);
+    }
 }

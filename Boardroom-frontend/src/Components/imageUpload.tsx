855da7194fd758c599ad331fe455305e2612787d
Janelle Tam <janelle.tam@mail.mcgill.ca>
2025-03-29 19:56:25 -0400

    add image upload component

---
+++ Boardroom-frontend/src/Components/imageUpload.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+interface FileUploadProps {
+  id: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FileUpload: React.FC<FileUploadProps> = ({ id, onChange }) => {
+  return (
+    <div className="flex items-center justify-center w-full">
+      <label
+        htmlFor={id}
+        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      >
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+          <svg
+            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+            <span className="font-semibold">Click to upload</span> or drag and drop
+          </p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">
+            PNG, JPEG, or JPG
+          </p>
+        </div>
+        <input
+          id={id}
+          type="file"
+          accept='.png, .jpg, .jpeg' // limit to images for now
+          className="hidden"
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default FileUpload;
--- Boardroom-frontend/src/pages/games/newGameForm.tsx
+++ Boardroom-frontend/src/pages/games/newGameForm.tsx
@@ -15,6 +15,15 @@ import {
   FormMessage
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import FileUpload from '@/components/imageUpload'
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -37,27 +46,43 @@ export function NewGameForm () {
     console.log(values)
   }
 
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      console.log('Selected file:', file.name);
+    }
+  }
+
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 font-roboto'>
-        <FormField
-          control={form.control}
-          name='username'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder='shadcn' {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type='submit'>Submit</Button>
-      </form>
-    </Form>
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <Card className='w-[500px] mx-auto mt-10 p-10'>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-8 font-roboto'
+          >
+            <FileUpload id='file_input' onChange={handleFileChange} />
+
+            <FormField
+              control={form.control}
+              name='username'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder='shadcn' {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit'>Submit</Button>
+          </form>
+        </Form>
+      </Card>
+    </div>
   )
 }

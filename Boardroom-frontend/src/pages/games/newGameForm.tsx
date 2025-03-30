cd72b1d7d38abc1f39eff06c25ff86df086a76d8
Janelle Tam <janelle.tam@mail.mcgill.ca>
2025-03-29 20:27:21 -0400

    finish ui for create game

--- Boardroom-frontend/src/pages/games/newGameForm.tsx
+++ Boardroom-frontend/src/pages/games/newGameForm.tsx
@@ -15,6 +15,7 @@ import {
   FormMessage
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import {
   Card,
   CardContent,
@@ -24,18 +25,27 @@ import {
   CardTitle
 } from '@/components/ui/card'
 import FileUpload from '@/components/imageUpload'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.'
-  })
+  title: z.string().min(1, { message: 'Title is required.' }),
+  image: z.instanceof(File).refine(image => !!image, {
+    message: 'A photo of the board game is required.'
+  }),
+  description: z.string().min(1, { message: 'Description is required.' }),
+  playersNeeded: z
+    .number()
+    .min(1, { message: 'Number of players needed is required.' })
 })
 
 export function NewGameForm () {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: ''
+      title: '',
+      image: undefined,
+      description: '',
+      playersNeeded: 1
     }
   })
 
@@ -46,42 +56,85 @@ export function NewGameForm () {
     console.log(values)
   }
 
-  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const file = event.target.files?.[0];
+  function handleFileChange (event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0]
     if (file) {
-      console.log('Selected file:', file.name);
+      console.log('Selected file:', file.name)
     }
   }
 
   return (
     <div className='h-screen w-screen flex justify-center items-center'>
-      <Card className='w-[500px] mx-auto mt-10 p-10'>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className='space-y-8 font-roboto'
-          >
-            <FileUpload id='file_input' onChange={handleFileChange} />
+      <Card className='w-[700px] mx-auto p-10'>
+        <CardTitle className='font-bold'>Add a new board game</CardTitle>
+        <ScrollArea className='h-[400px] w-full'>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className='space-y-8 font-roboto'
+            >
+              <FormField
+                control={form.control}
+                name='title'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Board Game Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Title of the board game' {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name='username'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder='shadcn' {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type='submit'>Submit</Button>
-          </form>
-        </Form>
+              <FormField
+                control={form.control}
+                name='image'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Image</FormLabel>
+                    <FormControl>
+                      <FileUpload id='file_input' onChange={handleFileChange} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name='description'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder='One to two line description of the board game'
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name='playersNeeded'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Minimum number of players needed</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='Minimum number of players needed'
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <Button type='submit'>Submit</Button>
+            </form>
+          </Form>
+        </ScrollArea>
       </Card>
     </div>
   )
